db75b347dd6e115e3a017db89ae05dcc5f5be849
Maksuda Yesmin <106536736+maksudaMou@users.noreply.github.com>
2022-09-28 00:40:42 +0600

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,31 @@
+﻿create table tags 
+(
+  tagid int not null identity primary key,
+  tag nvarchar (30) not null
+)
+go 
+create table publishers
+( 
+  publisherid int identity primary key,
+  publishername nvarchar (40) not null,
+  publisheremail nvarchar (50) null
+ )
+ go
+ create table authors
+ (
+	authorid int identity primary key,
+	authorname nvarchar(50) not null,
+	email nvarchar(50) null
+)
+go
+ create table books
+( 
+  bookid int identity primary key,
+  title nvarchar (40) not null,
+  coverprice money not null,
+  publishdate date not null,
+  available bit default 0,
+  Picture VARCHAR(150) NOT NULL,
+  publisherid int not null references publishers(publisherid)
+ )
+go
